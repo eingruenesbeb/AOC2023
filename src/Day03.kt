f486965f0aeb6db1e94ca802a2c62afb822dbd6b
Tim R <agreenbeb@gmail.com>
2023-12-03 23:46:23 +0100

    feat: Initial commit and solve day 3

---
+++ src/Day03.kt
@@ -0,0 +1,159 @@
+/**
+ * Represents an element in a grid.
+ */
+sealed interface GridElement
+
+/**
+ * Represents an empty grid-element.
+ */
+data object EmptyGridElement : GridElement
+
+/**
+ * Represents any grid-element, that is anything but an [EmptyGridElement] or [NumberGridElement].
+ */
+open class SymbolGridElement : GridElement
+
+/**
+ * Represents a gear grid-element.
+ */
+data class GearGridElement(val x: Int, val y: Int) : SymbolGridElement()
+
+/**
+ * Represents an integer grid-element.
+ *
+ * @property value The value of the integer.
+ */
+data class NumberGridElement(
+    val value: Int
+) : GridElement {
+    companion object {
+        /**
+         * Constructs a new [NumberGridElement]. The resulting value is based on what number (in base 10) is represented
+         * by the first digits in the given [CharArray].
+         *
+         * @param rightCharsInclusive The array of the characters including all digits at the beginning. (left to right).
+         *
+         * @return The resulting [NumberGridElement]. Its value is 0, when no suitable digits could be found at the
+         * beginning of [rightCharsInclusive].
+         */
+        fun constructRight(rightCharsInclusive: CharArray): NumberGridElement {
+            // Will be reversed at first.
+            var currentValue = ""
+            for (character in rightCharsInclusive) {
+                if (ValidDigits.isDigitChar(character)) currentValue += character else break
+            }
+
+            return if (currentValue == "") NumberGridElement(0) else NumberGridElement(currentValue.toInt())
+        }
+    }
+}
+
+data class HorizontalNumberRegion(
+    val xRange: IntRange,
+    val y: Int,
+    val localValue: Int
+)
+
+/**
+ * Represents a two-dimensional grid of finite size containing [GridElement]s at each point.
+ *
+ * @constructor Can be constructed from a string representation. This string has to be rectangular (same amount of lines
+ * as characters per line).
+ *
+ * A peculiarity is, that horizontally adjacent digits in the input string will result in every
+ * coordinate of these digits containing the [NumberGridElement] represented by them read from left to right.
+ */
+class TwoDimGrid(stringRows: List<String>) {
+    private val width: Int
+    private val height: Int
+    // The indices of the outer list are the y-index and the indices of the inner list are the x-indices.
+    private val elements: List<List<GridElement>>
+    // Due to the number-regions being horizontal, the y coordinate can be stored as a single int.
+    private val numberRegions: List<HorizontalNumberRegion>
+    private val gearElements: List<GearGridElement>
+
+    init {
+        val numberRegionsToConstruct = mutableListOf<HorizontalNumberRegion>()
+        val gearElementsToConstruct = mutableListOf<GearGridElement>()
+
+        width = stringRows.first().length
+        height = stringRows.size
+
+        /*
+         * The data will be stored with flipped axis internally. The get operator will invert this again to match the
+         * input.
+         */
+        elements = stringRows.mapIndexed yLoop@ { yIndex, line ->
+            if (line.length != width) throw IllegalArgumentException("Input isn't rectangular.")
+
+            var skipUntilIndex = 0
+            var lastNumberGridElement = NumberGridElement(0)  // Placeholder value
+            line.toCharArray().mapIndexed xLoop@ { xIndex, char ->
+                if (xIndex < skipUntilIndex) return@xLoop lastNumberGridElement
+                when (char) {
+                    '.' -> EmptyGridElement
+                    '*' -> GearGridElement(xIndex, yIndex).also {
+                        gearElementsToConstruct.add(it)
+                    }
+                    in '0'..'9' -> {
+                        lastNumberGridElement = NumberGridElement.constructRight(line.toCharArray(startIndex = xIndex))
+                        skipUntilIndex = xIndex + lastNumberGridElement.value.toString().length
+
+                        numberRegionsToConstruct.add(
+                            HorizontalNumberRegion(
+                                xIndex..<skipUntilIndex,
+                                yIndex,
+                                lastNumberGridElement.value
+                            )
+                        )
+
+                        lastNumberGridElement
+                    }
+                    else -> SymbolGridElement()
+                }
+            }
+        }
+
+        numberRegions = numberRegionsToConstruct.toList()
+        gearElements = gearElementsToConstruct.toList()
+    }
+
+    operator fun get(indexX: Int, indexY: Int) = elements[indexY][indexX]
+
+    fun getNumberRegionsNextToSymbol() = numberRegions.filter {
+        val xRangeToSearch = (it.xRange.first - 1).coerceIn(0, width - 1)..(it.xRange.last + 1).coerceIn(0, width - 1)
+        val yRangeToSearch = (it.y - 1).coerceIn(0, height - 1)..(it.y + 1).coerceIn(0, height - 1)
+
+        return@filter yRangeToSearch.any { y -> xRangeToSearch.any { x -> this[x, y] is SymbolGridElement } }
+    }
+
+    fun gearRatios() = gearElements.map { gear ->
+        val xRange = (gear.x - 1).coerceIn(0, width - 1)..(gear.x + 1).coerceIn(0, width - 1)
+        val yRange = (gear.y - 1).coerceIn(0, height - 1)..(gear.y + 1).coerceIn(0, height - 1)
+
+        val adjacentNumberRegions = numberRegions.filter { it.xRange.any { x -> x in xRange } && it.y in yRange }
+
+        if (adjacentNumberRegions.size < 2) return@map 0
+
+        adjacentNumberRegions.fold(1) { acc, region -> acc * region.localValue }
+    }
+}
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return TwoDimGrid(input).getNumberRegionsNextToSymbol().fold(0) { acc, region -> acc + region.localValue }
+    }
+
+    fun part2(input: List<String>): Int {
+        return TwoDimGrid(input).gearRatios().fold(0) { acc, ratio -> acc + ratio }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 4361)
+    check(part2(testInput) == 467835)
+
+    val input = readInput("Day03")
+    part1(input).println()
+    part2(input).println()
+}
--- src/Utils.kt
+++ src/Utils.kt
@@ -1,3 +1,5 @@
+@file:Suppress("MemberVisibilityCanBePrivate", "unused")
+
 import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
@@ -19,3 +21,57 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+enum class ValidDigits {
+    ZERO,
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE;
+
+    fun stringRepresentations() = when (this) {
+        ZERO -> listOf("0", "zero")
+        ONE -> listOf("1", "one")
+        TWO -> listOf("2", "two")
+        THREE -> listOf("3", "three")
+        FOUR -> listOf("4", "four")
+        FIVE -> listOf("5", "five")
+        SIX -> listOf("6", "six")
+        SEVEN -> listOf("7", "seven")
+        EIGHT -> listOf("8", "eight")
+        NINE -> listOf("9", "nine")
+    }
+
+    companion object {
+        fun fromStringRepresentation(input: String) = when(input) {
+            in ZERO.stringRepresentations() -> ZERO.ordinal
+            in ONE.stringRepresentations() -> ONE.ordinal
+            in TWO.stringRepresentations() -> TWO.ordinal
+            in THREE.stringRepresentations() -> THREE.ordinal
+            in FOUR.stringRepresentations() -> FOUR.ordinal
+            in FIVE.stringRepresentations() -> FIVE.ordinal
+            in SIX.stringRepresentations() -> SIX.ordinal
+            in SEVEN.stringRepresentations() -> SEVEN.ordinal
+            in EIGHT.stringRepresentations() -> EIGHT.ordinal
+            in NINE.stringRepresentations() -> NINE.ordinal
+            else -> null
+        }
+
+        fun fromChar(input: Char) = fromStringRepresentation(input.toString())
+
+        fun isDigit(input: String) = fromStringRepresentation(input) != null
+
+        fun isDigitChar(input: Char) = isDigit(input.toString())
+
+        fun allRepresentations() = buildList {
+            ValidDigits.entries.forEach {
+                this.addAll(it.stringRepresentations())
+            }
+        }
+    }
+}
