f3fb3b4e3ea81073e292116d2e48cba93ffddc68
Tim R <agreenbeb@gmail.com>
2023-12-17 19:49:54 +0100

    Solved day 11

--- src/SparseTwoDimGrid.kt
+++ src/SparseTwoDimGrid.kt
@@ -1,5 +1,8 @@
 /**
  * Represents a [TwoDimGrid], but it contains `null` elements, which aren't contained in the list of elements.
+ *
+ * The grid class used as grid-elements should always have a representative for an empty of zero element.
+ * (As a kind of "typed-null")
  */
 interface SparseTwoDimGrid<T: TwoDimGrid.GridElement> {
     val elements: List<TwoDimensionalIndexedValue<T>>
